71b3210c7c1110576c65ed25dc25425228e6b4b4
Shannon Roberts <bklynsurfer26@gmail.com>
2017-05-14 15:51:23 -0400

    add radio buttons-not working

--- public/js/init.js
+++ public/js/init.js
@@ -13,52 +13,31 @@
   // $(document).ready(function() {
   // });
 
-// var compareName = data.child.kid_name;
-// var getNameInfo = function(compareName){
-$(input#input_milk).on("focus", function(){
-  $(checkbox#right).show();
-});
 
-// $.ajax({
-//   url: "https://www.behindthename.com/api/lookup.php?name=" + compareName +"&key=sh111457",
-//  method: "GET",
-//   success: function(data) {
-//     console.log(data.name);
-//   }
-// });
-// }; //end of getNameInfo
-// console.log(getNameInfo);
-// var xhr = new XMLHttpRequest();
-// xhr.open("GET",  "https://www.behindthename.com/api/lookup.php?name=mary&key=sh111457", false);
-// xhr.setRequestHeader('Content-Type', 'text/xml');
-// xhr.send();
+// $("#info").on("click", function(){
+//     console.log("hello");
+//     $.ajax({
+//       url: 'https://uinames.com/api/',
+//       dataType: 'json',
+//       success: function(data){
+//       /* Act on the event */
+//         console.log(data);
+//         $("#info").append(data.name);
+//       }
+//     })
 
-// xmlDocument = xhr.responseXML;
-// console.log(xhr.status);
-// console.log(xhr.statusText);
-// console.log(xmlDocument.childNodes['0'].textContent);
-// //add a click event
-// $("p#info").on("click", function(){
-//   console.log("hello api");
-// });
-$("#info").on("click", function(){
-    console.log("hello");
-    $.ajax({
-      url: 'https://uinames.com/api/',
-      dataType: 'json',
-      success: function(data){
-      /* Act on the event */
-        console.log(data);
-        $("#info").append(data.name);
-      }
-    })
+//   });//end uninames.com
 
-  });//end uninames.com
-
-  // $('.datepicker').pickadate({
-  //   selectMonths: true, // Creates a dropdown to control month
-  //   selectYears: 15 // Creates a dropdown of 15 years to control year
-  // });
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
 
   }); // end of document ready
+// $("input#input_milk").on("focus", function(){
+//   console.log("milk focus");
+//   $(radio#radio_right).show();
+// });
+
+
 })(jQuery); // end of jQuery name space
